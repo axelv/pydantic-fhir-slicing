8b9daa9ce4217f34480d351f085f29922d66c6e3
Axel Vanraes <axel.vanraes@tiro.health>
2025-03-14 21:24:37 +0100

    fix cardinality checks

--- fhir_slicing/slice.py
+++ fhir_slicing/slice.py
@@ -1,7 +1,5 @@
 from functools import partial
-from typing import Annotated, Any, Container, Iterable, List, Literal, TypeGuard, TypeVar, overload
-
-import annotated_types
+from typing import Any, Container, Iterable, List, Literal, TypeGuard, TypeVar, overload
 
 from fhir_slicing.typing import ElementArray
 
@@ -9,17 +7,20 @@ TValueType = TypeVar("TValueType", covariant=True)
 
 
 class BaseSlice[TValueType]:
-    def __init__(self, min_items: int = 0, max_items: int | Literal["*"] = "*"):
+    def __init__(self, min_items: int = 0, max_items: int | None = None):
         self.min_items = min_items
         self.max_items = max_items
 
+    def __repr__(self):
+        return f"{self.__class__.__name__}(min_items={self.min_items}, max_items={self.max_items})"
+
     def __set_name__(self, owner: ElementArray[Any], name: str):
-        self.name = name
+        self.slice_name = name if name != "_" else "@default"
         self.filter_elements: partial[Iterable[TValueType]] = partial(
-            owner.filter_elements_for_slice, slice_name=self.name
+            owner.filter_elements_for_slice, slice_name=self.slice_name
         )
         self.is_element_part_of_slice: partial[TypeGuard[TValueType]] = partial(
-            owner.is_element_part_of_slice, slice_name=self.name
+            owner.is_element_part_of_slice, slice_name=self.slice_name
         )
 
 
@@ -28,7 +29,7 @@ class Slice[TValueType](BaseSlice[TValueType]):
         try:
             return next(iter(self.filter_elements(obj)))
         except StopIteration:
-            raise ValueError(f"No value for slice '{self.name}'.")
+            raise ValueError(f"No value for slice '{self.slice_name}'.")
 
     def __set__(self, obj: ElementArray[TValueType], element: Any):
         for index, old_element in enumerate(obj):
@@ -39,15 +40,6 @@ class Slice[TValueType](BaseSlice[TValueType]):
 
 
 class OptionalSlice[TValueType](BaseSlice[TValueType]):
-    def __set_name__(self, owner: ElementArray[Any], name: str):
-        self.name = name
-        self.filter_elements: partial[Iterable[TValueType]] = partial(
-            owner.filter_elements_for_slice, slice_name=self.name
-        )
-        self.is_element_part_of_slice: partial[TypeGuard[TValueType]] = partial(
-            owner.is_element_part_of_slice, slice_name=self.name
-        )
-
     def __get__(self, obj: ElementArray[Any], objtype: type[Container] | None = None) -> TValueType | None:
         return next(iter(self.filter_elements(obj)), None)
 
@@ -59,15 +51,6 @@ class OptionalSlice[TValueType](BaseSlice[TValueType]):
 
 
 class SliceList[TValueType](BaseSlice[TValueType]):
-    def __set_name__(self, owner: ElementArray[Any], name: str):
-        self.name = name
-        self.filter_elements: partial[Iterable[TValueType]] = partial(
-            owner.filter_elements_for_slice, slice_name=self.name
-        )
-        self.is_element_part_of_slice: partial[TypeGuard[TValueType]] = partial(
-            owner.is_element_part_of_slice, slice_name=self.name
-        )
-
     def __get__(self, obj: ElementArray[Any], objtype: type[Container] | None = None) -> List[TValueType]:
         return [*self.filter_elements(obj)]
 
@@ -75,24 +58,38 @@ class SliceList[TValueType](BaseSlice[TValueType]):
         raise NotImplementedError("Cannot set value on slice list.")
 
 
-NonZeroPositiveInt = Annotated[int, annotated_types.Gt(0)]
-
-
 @overload
-def slice(min: Literal[0], max: Literal[1]) -> OptionalSlice: ...
+def slice(min_items: Literal[0], max_items: Literal[1]) -> OptionalSlice: ...
 @overload
-def slice(min: Literal[1], max: Literal[1]) -> Slice: ...
+def slice(min_items: Literal[1], max_items: Literal[1]) -> Slice: ...
 @overload
-def slice(min: NonZeroPositiveInt, max: Literal["*"]) -> SliceList: ...
-def slice(min: int, max: int | Literal["*"]):
-    match (min, max):
+def slice(min_items: Literal[0], max_items: Literal["*"]) -> SliceList: ...
+@overload
+def slice(min_items: Literal[1], max_items: Literal["*"]) -> SliceList: ...
+@overload
+def slice(min_items: int, max_items: int | Literal["*"]) -> Any: ...
+def slice(min_items: int, max_items: int | Literal["*"]):
+    match (min_items, max_items):
         case (0, 1):
             return OptionalSlice(min_items=0, max_items=1)
         case (1, 1):
             return Slice(min_items=1, max_items=1)
         case (0, "*"):
-            return SliceList(min_items=0, max_items="*")
+            return SliceList(min_items=0)
         case (1, "*"):
-            return SliceList(min_items=0, max_items="*")
+            return SliceList(min_items=1)
+        case (int(), "*"):
+            return SliceList(min_items=min_items)
+        case (int(), int()):
+            return SliceList(min_items=min_items)
         case _:
-            raise ValueError("Invalid slice cardinality.")
+            raise ValueError(f"Invalid slice specification: {min_items}..{max_items}")
+
+
+def slice_validator(elements: ElementArray, *, slice_name: str, slice_descriptor: BaseSlice):
+    num_items = len(list(slice_descriptor.filter_elements(elements)))
+    if slice_descriptor.max_items is not None and num_items > slice_descriptor.max_items:
+        raise ValueError(f"Too many items in slice '{slice_name}'")
+    elif num_items < slice_descriptor.min_items:
+        raise ValueError(f"Not enough items in slice '{slice_name}'")
+    return elements
